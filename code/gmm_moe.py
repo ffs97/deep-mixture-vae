f01336be5bf1e0346e4e73f0eee33d11eb00995b
abhipanda4 <abhipanda03@gmail.com>
2018-11-09 21:00:09 +0530

    revert here if anyhting breaks

--- code/gmm_moe.py
+++ code/gmm_moe.py
@@ -7,6 +7,7 @@ from includes.config import Config
 
 from tensorflow.examples.tutorials.mnist import input_data
 from sklearn.mixture import GaussianMixture
+from tqdm import tqdm
 
 class GMMMoE:
     def __init__(self, N, M, K, alpha=None, mean=None, sigma=None):
@@ -19,28 +20,45 @@ class GMMMoE:
         self.M = M
         self.K = K
 
-        # print(alpha.shape, mean.shape, sigma.shape)
         self.alphas = alpha[np.newaxis, :]
         self.means = mean
+        # self.sigmas = np.array([np.identity(M) for _ in range(K)])
         self.sigmas = sigma
 
         self.phis = np.zeros((K, M, K))
 
     def gaussian_pdf(self, x, mu, sigma):
-        # diff = x - mu
-        pass
+        '''
+        x -> shape is (n, d)
+        mu -> shape is either (n, d) or (1, d)
+        sigma -> (d, d)
+        '''
+        diff = x - mu
+        sigma_inv = np.linalg.inv(sigma)
+        prob = np.sum(np.multiply(np.dot(diff, sigma_inv), diff), axis=1)
+        denominator = np.sqrt(np.power(2 * math.pi, self.M) * abs(np.linalg.det(sigma)))
+        return np.exp(-0.5 * prob) / denominator
 
 
     def E_step(self, X, Y):
+        '''
+        Shape of X: (num_samples X num_dimensions) = (N, M)
+        Shape of Y: (num_samples X num_classes)    = (N, K)
+
+        returns H -> (num_samples X num_classes)   = (N, K)
+        '''
+
         H = np.zeros((self.N, self.K))
 
         for e in range(self.K):
             # compute P(X | v_j)
-            p_x_v = self.gaussian_pdf(X, self.means[e, :, None], self.sigmas[e, :, :])
+            p_x_v = self.gaussian_pdf(X, self.means[e, None, :], self.sigmas[e, :, :]).reshape(self.N, 1)
 
             # compute P(Y | X, theta_j)
             Y_pred = np.dot(X, self.phis[e, :, :])
-            p_y_x = np.mean((Y - Y_pred)**2, axis=1, keepdims=True)
+            p_y_x = self.gaussian_pdf(Y_pred, Y, np.identity(self.K)).reshape(self.N, 1)
+            # prob = np.exp(-0.5 * (Y - Y_pred)**2) / np.sqrt(2 * math.pi)
+            # p_y_x = np.mean(prob, axis=1, keepdims=True)
 
             assert p_x_v.shape == p_y_x.shape
 
@@ -48,7 +66,7 @@ class GMMMoE:
             H[:, e] = h[0, :]
 
         # normalize along all experts
-        sum_along_rows = np.sum(H, axis=1, keepdims=True) + 10e-4
+        sum_along_rows = np.sum(H, axis=1, keepdims=True) + 10e-3
         H = H / sum_along_rows
         return H
 
@@ -58,7 +76,7 @@ class GMMMoE:
 
         for e in range(self.K):
             # mu estimate
-            mu_e = np.sum(np.multiply(X, H[:, None, e]), axis=0, keepdims=True) / H_sum[:, e]
+            mu_e = np.sum(np.multiply(X, H[:, None, e]), axis=0, keepdims=True) / (H_sum[:, e] + 10e-3)
 
             # sigma estimate
             diff = X - mu_e
@@ -75,32 +93,25 @@ class GMMMoE:
             self.phis[e, :] = phi_e
 
     def fit(self, X, Y, max_iter=100):
-        for idx in range(max_iter):
-            print(idx)
+        for idx in tqdm(range(max_iter)):
             H = self.E_step(X, Y)
             self.M_step(H, X, Y)
 
 if __name__ == "__main__":
-    mnist = input_data.read_data_sets("data/mnist/", one_hot=True)
-    X_train = mnist.train.images
-    Y_train = mnist.train.labels
-
-    N = 1000
-    X = np.array(X_train[: N]).T
-    Y = np.array(Y_train[: N]).T
-    M = 784
+    N = 100
+    M = 15
     K = 10
-
-    gmm = GaussianMixture(10)
-    gmm.fit(X, Y)
-    alpha, mu, sigma = gmm.weights_, gmm.means_, gmm.covariances_
-
-    model = GMMMoE(N, M, K, alpha, mu, sigma)
     X = np.random.normal(size=(N, M))
     Y = np.random.randint(0, K, N)
     Y_binary = np.zeros((N, K))
-
     for i in range(N):
         Y_binary[i][Y[i]] = 1
 
+    # https://www.kaggle.com/danielhanchen/gaussian-mixture-models-on-mnist-iris#
+    gmm = GaussianMixture(n_components=K, init_params='kmeans',
+           n_init=5, max_iter=5000)
+    gmm.fit(X, Y_binary)
+    alpha, mu, sigma = gmm.weights_, gmm.means_, gmm.covariances_
+
+    model = GMMMoE(N, M, K, alpha, mu, sigma)
     model.fit(X, Y_binary)
