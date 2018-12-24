8cf60c70578c4cfe36b9b8600b477025bfc6a57c
Abhisek Panda <apanda@gpu.cse.iitk.ac.in>
2018-12-24 22:53:39 +0530

    separate network for batchnorm added

--- code/includes/network.py
+++ code/includes/network.py
@@ -11,39 +11,20 @@ class FeedForwardNetwork:
         self.activation = activation
         self.initializer = initializer
 
-    def build(self, output_dims, layer_sizes, input_var, use_bn=False, is_training=True, reuse=False):
+    def build(self, output_dims, layer_sizes, input_var, reuse=False):
         layers = []
         with tf.variable_scope(self.name, reuse=reuse) as _:
             input_var = tf.layers.flatten(input_var)
-
-            if use_bn:
-                for index, layer_size in enumerate(layer_sizes):
-                    layers.append(
-                        tf.layers.dense(
-                            input_var if index == 0 else layers[index - 1],
-                            layer_size,
-                            activation=self.activation,
-                            kernel_initializer=self.initializer(),
-                            name="network_layer_" + str(index + 1)
-                        )
+            for index, layer_size in enumerate(layer_sizes):
+                layers.append(
+                    tf.layers.dense(
+                        input_var if index == 0 else layers[index - 1],
+                        layer_size,
+                        activation=self.activation,
+                        kernel_initializer=self.initializer(),
+                        name="network_layer_" + str(index + 1)
                     )
-                    layers.append(
-                        tf.layers.batch_normalization(
-                            layers[index],
-                            name="batchnorm_layer_" + str(index + 1)
-                        )
-                    )
-            else:
-                for index, layer_size in enumerate(layer_sizes):
-                    layers.append(
-                        tf.layers.dense(
-                            input_var if index == 0 else layers[index - 1],
-                            layer_size,
-                            activation=self.activation,
-                            kernel_initializer=self.initializer(),
-                            name="network_layer_" + str(index + 1)
-                        )
-                    )
+                )
 
             self.outputs = []
             for name, output_dim in output_dims:
@@ -63,15 +44,51 @@ class FeedForwardNetwork:
 
         return self.outputs
 
-    # def get_weight_decay_loss(self):
-    #     params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+class FeedForwardNetworkWithBN:
+    def __init__(self, name, activation=tf.nn.relu, initializer=tf.contrib.layers.xavier_initializer, dropout=0.0):
+        self.name = name
+        self.keep_prob = 1 - dropout
 
-    #     r1 = self.name + "\/.*\/kernel"
-    #     r2 = self.name + "\/.*\/gamma"
+        self.activation = activation
+        self.initializer = initializer
 
-    #     l2_norm_loss = 0
-    #     for p in params:
-    #         if re.search(r1, p.name) or re.search(r2, p.name):
-    #             l2_norm_loss += tf.nn.l2_loss(p)
+    def build(self, output_dims, layer_sizes, input_var, is_training=True, reuse=False):
+        layers = []
+        with tf.variable_scope(self.name, reuse=reuse) as _:
+            input_var = tf.layers.flatten(input_var)
 
-    #     return self.weight_decay_coeff * l2_norm_loss
+            for index, layer_size in enumerate(layer_sizes):
+                layers.append(
+                    tf.layers.dense(
+                        input_var if index == 0 else layers[index - 1],
+                        layer_size,
+                        activation=self.activation,
+                        kernel_initializer=self.initializer(),
+                        name="network_layer_" + str(index + 1)
+                    )
+                )
+                layers.append(
+                    tf.layers.batch_normalization(
+                        layers[index],
+                        training=is_training,
+                        name="batchnorm_layer_" + str(index + 1)
+                    )
+                )
+
+            self.outputs = []
+            for name, output_dim in output_dims:
+                self.outputs.append(
+                    tf.layers.dense(
+                        layers[-1],
+                        output_dim,
+                        kernel_initializer=self.initializer(),
+                        name="network_output/" + name
+                    )
+                )
+
+        self.layers = layers
+
+        if len(self.outputs) == 1:
+            return self.outputs[0]
+
+        return self.outputs
